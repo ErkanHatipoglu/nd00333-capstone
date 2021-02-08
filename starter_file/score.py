e7a7a7dd42e3649994b6faedf26416f77e9edfe2
Erkan Hatipoğlu <38298853+ErkanHatipoglu@users.noreply.github.com>
2021-02-08 00:40:56 +0000

    Add files via upload

--- starter_file/score.py
+++ starter_file/score.py
@@ -1,11 +1,8 @@
 import joblib
 import numpy as np
+import pandas as pd
 import os
 import json
-
-from inference_schema.schema_decorators import input_schema, output_schema
-from inference_schema.parameter_types.numpy_parameter_type import NumpyParameterType
-
 
 # The init() method is called once, when the web service starts up.
 #
@@ -24,16 +21,30 @@ def init():
 
 # The run() method is called each time a request is made to the scoring API.
 #
-# Shown here are the optional input_schema and output_schema decorators
-# from the inference-schema pip package. Using these decorators on your
-# run() method parses and validates the incoming payload against
-# the example input you provide here. This will also generate a Swagger
-# API document for your web service.
-@input_schema('data', NumpyParameterType(np.array([[ -0.0859528613529813,-0.36354521171325305,-0.09848942592492069,-0.2796986280674258,-0.7578351616129093,1.0756335865128208,-0.12905680819293966,1.339259538474412,-0.26835014340179053,3.933471146038076,-1.0033203374478632,-0.07333340857320904,-1.2623323347145659,-1.554030619560426,0.2925864878985621,-0.7682554384315856,-1.0374764641885903,-0.2095969950992871,-2.297476708819124,-0.5060972419158338,-0.7947788299373657,-0.26093173135248043,0.5539305114736647,1.2487482387683566,1.0417709664249128,-0.45567727750211645,-0.7955454757135164,-0.24546682664767563,0.31229303496618394,1.1188800229146632,-0.07919625766046033,-1.3559196129494377,2.08580400027604,0.22052234535891524,-0.5768383662665258,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0,1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0,1.0,0.0,0.0,0.0]])))
-@output_schema(NumpyParameterType(np.array([1.98900000e+05])))
 def run(data):
-    # Use the model object loaded by init().
-    result = model.predict(data)
+    print("data before")
+    print(data)
+    data=NumpyParameterType(np.array([data])))
+    print("data after")
+    print(data)
+    names = [i for i in range(398)]
+    df = pd.DataFrame(data=np.concatenate(data), index=None, columns=names)
+    print("df:")
+    print(df.head)
+    try:
+        #data=np.array(data)
+        names = [i for i in range(398)]
+        df = pd.DataFrame(data=data, index=None, columns=names)
+        print("df:")
+        print(df.head)
 
-    # You can return any JSON-serializable object.
-    return result.tolist()
+
+        # Use the model object loaded by init().
+        result = model.predict(df)
+        print(result)
+        # You can return any JSON-serializable object.
+        return result.tolist()
+    except Exception as e:
+        result = str(e)
+        # return error message back to the client
+        return json.dumps({"error": result})
